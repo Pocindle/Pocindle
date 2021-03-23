014e36c08dc7778b71da9369ab2d41787a155e5e
Vladislav Prekel <misterptits@yandex.ru>
2021-03-23 19:44:13 +0700

    Some types

--- Pocindle.Domain/Domain.fs
+++ Pocindle.Domain/Domain.fs
@@ -1,3 +1,30 @@
 ﻿module Pocindle.Domain
 
-type UserId = UserId of int64
+open System.Net.Mail
+
+type Undefined = exn
+
+type UserId = UserId of uint64
+
+type User = { UserId: UserId }
+
+type KindleEmailAddress = KindleEmailAddress of MailAddress
+
+type Article = Article of Undefined
+
+type Delivery =
+    { UserId: UserId
+      Data: Undefined
+      To: KindleEmailAddress }
+
+type EpubFile = Undefined
+
+type ConvertError =
+    | NoPandoc
+    | NoCalibre
+    | Other of Undefined
+
+type WebToEpubConverter = | Pandoc of Undefined
+                          | Other of Undefined
+
+type ConvertWebToEpub = WebToEpubConverter -> Article -> Async<Result<EpubFile, ConvertError>>
