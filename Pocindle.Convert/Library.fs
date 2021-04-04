75c95fde0d1596de77294f5ddfc829fcd85ac3f0
Vladislav Prekel <misterptits@yandex.ru>
2021-04-04 15:44:52 +0700

    taskResult instead of task

--- Pocindle.Convert/Library.fs
+++ Pocindle.Convert/Library.fs
@@ -2,18 +2,19 @@
 
 open System.IO
 open FSharp.Control.Tasks
+open FsToolkit.ErrorHandling
 open SimpleExec
 
 open Pocindle.Convert.Domain
 
-let convertToEpub: ConvertWebToEpub =
+let convertToEpub : ConvertWebToEpub =
     fun converter (Article article) ->
-        task {
+        taskResult {
             match converter with
             | Pandoc ->
                 try
                     do! Command.RunAsync("pandoc", article.AbsoluteUri + " -o qwe1.epub")
-                    return "qwe1.epub" |> EpubFile |> Ok
-                with ex -> return ex |> OtherError |> Error
-            | _ -> return Error NoPandoc
+                    return "qwe1.epub" |> EpubFile
+                with ex -> return! ex |> OtherError |> Error
+            | _ -> return! Error NoPandoc
         }
