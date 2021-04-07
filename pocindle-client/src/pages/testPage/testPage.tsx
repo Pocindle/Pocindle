0731ce70deb0faefbfb645d5e37209648fc2858f
Alexey Panov <lexade1802@gmail.com>
2021-04-07 20:14:04 +0700

    private route

--- pocindle-client/src/pages/testPage/testPage.tsx
+++ pocindle-client/src/pages/testPage/testPage.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import MainLayout from '../../layouts/mainLayout/mainLayout';
+import { useRouteMatch, useLocation } from 'react-router-dom';
 import { TestComponent } from '../../components';
 
 const TestPage: React.FC = () => {
+  const match = useRouteMatch();
+  const location = useLocation();
+  console.log('match', match);
+  console.log('location', location);
+
   return (
     <React.Fragment>
       <MainLayout>
