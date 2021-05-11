f5e7b8f71b380b355a5a80f1dbc45acb589a1dad
Alexey Panov <lexade1802@gmail.com>
2021-05-11 18:42:33 +0700

    add styles to authorizationFinishedPage, refactor styles for authorizationPage

--- pocindle-client/src/pages/authorizationFinishedPage/authorizationFinishedPage.tsx
+++ pocindle-client/src/pages/authorizationFinishedPage/authorizationFinishedPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { AuthorizationLayout } from '../../layouts';
 import { postRequestToken } from '../../api/apiRequests';
 import './authorizationFinishedPage.scss';
@@ -26,14 +26,31 @@ const AuthorizationFinishedPage: React.FC = () => {
         <div className="authorization-finished-page__wrapper">
           <div className="authorization-finished-page__message message">
             <div className="message__content">
-              {typeof isSuccessful === null ? (
-                <div>Waiting for result</div>
-              ) : isSuccessful ? (
-                <div>success</div>
+              {typeof isSuccessful === 'object' ? (
+                <span className="message__status">Waiting for result...</span>
+              ) : isSuccessful === true ? (
+                <React.Fragment>
+                  <span className="message__status">
+                    Authorization is successful!
+                  </span>
+                  <span className="message__redirect">
+                    {'Go to '}
+                    <Link to="/" className="message__link">
+                      Pocindle
+                    </Link>
+                  </span>
+                </React.Fragment>
               ) : (
-                <div>error</div>
+                <React.Fragment>
+                  <span className="message__status">Authorization error!</span>
+                  <span className="message__redirect">
+                    {'Return to '}
+                    <Link to="/auth" className="message__link">
+                      Authentification Page
+                    </Link>
+                  </span>
+                </React.Fragment>
               )}
-              <span>{`Message: ${requestToken}`}</span>
             </div>
           </div>
         </div>
