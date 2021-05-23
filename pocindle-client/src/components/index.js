3c0d5efb3b41eff5f8839d5ade39ae6840f094d4
Alexey Panov <lexade1802@gmail.com>
2021-05-23 20:24:14 +0700

    change index.js to index.ts

--- pocindle-client/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import TestComponent from './testComponent/testComponent';
-import Navbar from './navbar/navbar';
-import Footer from './footer/footer';
-import ArticleCard from './articleCard/articleCard';
-
-export { TestComponent, Navbar, Footer, ArticleCard };
