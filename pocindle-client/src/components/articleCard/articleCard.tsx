387e83a2c0a1f6815aea8c2d0a0e718df7aa2f99
Alexey Panov <lexade1802@gmail.com>
2021-05-23 20:18:19 +0700

    implement intermediate articleCard

--- pocindle-client/src/components/articleCard/articleCard.tsx
+++ pocindle-client/src/components/articleCard/articleCard.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import './articleCard.scss';
 
-const ArticleCard: React.FC = () => {
-  return <div>ArticleCard</div>;
+interface article {
+  url: string;
+  title: string;
+  excerpt?: string;
+  listenDurationEstimate?: number;
+  wordCount?: number;
+}
+
+export type { article };
+
+const ArticleCard: React.FC<article> = ({
+  url,
+  title,
+  excerpt,
+  listenDurationEstimate,
+  wordCount,
+}) => {
+  return (
+    <div className="article-card">
+      <div className="article-card__wrapper">
+        <div className="article-card__item">{title}</div>
+        <div className="article-card__item">{url}</div>
+        <div className="article-card__item">{excerpt}</div>
+        <div className="article-card__item">{listenDurationEstimate}</div>
+        <div className="article-card__item">{wordCount}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ArticleCard;
